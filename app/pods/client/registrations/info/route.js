a9de94684f7cebb9813b613fcdd096c39d7f1d60
Jim <jim.jenkins.iv@gmail.com>
2016-08-25 00:09:50 -0400

    wrap up registration wizard
    started on basic browsing

--- app/pods/client/registrations/info/route.js
+++ app/pods/client/registrations/info/route.js
@@ -3,12 +3,19 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function (params) {
     return Ember.RSVP.hash({
-      model: this.store.query('registration', {id: params.registration_id, with: 'attendees,requests'}),
+      model: this.store.query('registration', {id: params.registration_id, with: 'attendees'}),
+      requests: this.store.query('request', {registration_id: params.registration_id, with: 'all'}),
       charges: this.store.query('charge', {registration_id: params.registration_id})
     });
   },
 
   setupController: function (controller, resolved) {
+    var self = this;
+    // get the full event here since we need to side load in event details
+    resolved.requests.forEach(function (item) {
+      self.store.findRecord('event', item.get('event.id'), {include: 'all'});
+    });
+
     var model = resolved.model.get('firstObject');
     this._super(controller, model);
   }
--- app/pods/client/registrations/new/route.js
+++ app/pods/client/registrations/new/route.js
@@ -15,7 +15,6 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, Error, {
   model: function (params) {
     var accountId = this.get('currentAccount.id');
     return Ember.RSVP.hash({
-      registration: this.store.createRecord('registration', {}),
       attendees: this.store.query('attendee', {account_id: accountId}),
       locations: this.store.findAll('location'),
       programs: this.store.findAll('program'),
@@ -27,16 +26,20 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, Error, {
   },
 
   /**
-   * pass attendees to controller
+   * reset key variables each time the page is loaded
+   *
    * @param controller
    * @param resolved
    */
   setupController: function (controller, resolved) {
+    // setup main registration
+    resolved.registration = this.store.createRecord('registration', {});
     this._super(controller, resolved);
 
     // seed wizard with a single request
-    var newRequest = this.store.createRecord('request', {priority: 1, registration: resolved.registration});
-    controller.get('newRequests').pushObject(newRequest);
+    let newRequests = [];
+    newRequests.pushObject(this.store.createRecord('request', {priority: 1, registration: resolved.registration}));
+    controller.set('newRequests', newRequests);
   },
 
   actions: {
@@ -92,68 +95,33 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, Error, {
      */
     save: function () {
       var self = this;
-
-      //first save a registration
-      // var data = {
-      //   attendee: this.get('registration.camper'),
-      //   notes: this.get('registration.registrationNote')
-      // };
-      // var newRegistration = this.store.createRecord('registration', data);
       var newRegistration = this.controller.get('model.registration');
 
+      // all the requests that need saving
       var subItems = [];
 
-      // a function to save a sub-item
-      var subItemSave = function (item) {
-        //console.log('save updated record');
-        return item.save();
-      };
-
-      /**
-       * success handler for save action
-       * @param post
-       */
-      function success(post) {
-        var that = self;
-
+      // it's go time, perform the save
+      newRegistration.save().then((post)=> {
         //now add requests for the registration
-        // var requests = self.controller.get('registration.requests');
         var requestCount = newRegistration.get('requests').length;
         var registrationId = post.get('id');
 
         newRegistration.get('requests').forEach(function (item) {
-          // var data = {
-          //   event: item.get('event'),
-          //   registration: post,
-          //   priority: item.get('priority'),
-          //   note: item.get('note')
-          // };
-          // var request = self.store.createRecord('request', data);
-          subItems.push(subItemSave(item));
+          subItems.push(item.save());
         }, this);
 
         Ember.RSVP.all(subItems).then(function () {
-          // reset registration wizard
-          that.get('registration').resetRegistration();
+          // get out of here!
           self.get('notify').success('Success saving registration including ' + requestCount + ' individual requests.');
-          self.transitionToRoute('registrations.info', registrationId);
-        }, failure);
-      }
+          self.transitionTo('client.registrations.info', registrationId);
+        }, (reason) => {
+          self.handleFormError(reason);
+        });
+      }, (reason) => {
+        this.handleFormError(reason);
+      });
 
-      /**
-       * handle an error in saving the new registration record
-       * @param reason
-       */
-      function failure(reason) {
-        var errorHTML = '<strong>' + reason.responseJSON.records.userMessage + '</strong> <br/>';
-        reason.responseJSON.records.validationList.forEach(function (item) {
-          errorHTML = errorHTML + item.message + '<br/>';
-        }, this);
-        self.get('notify').warning({raw: errorHTML});
-      }
 
-      // it's go time, perform the save
-      newRegistration.save().then(success, failure);
     } // end save function
 
 
